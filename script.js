fb568623362a0f685a84764916a26e93e410d8c6
aabenjamim <alanebenjamimdossantos@gmail.com>
2022-10-30 02:13:25 -0300

    Versão pegando as mensagens do servidor

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // let receitas = []; // pegar essas receitas que já foram salvas na internet e trazer pra cá 
 
-let usuarios = [];
-let mensagens = [];
+let dataAtual = new Date()
+let hora = dataAtual.getHours();
+let minuto = dataAtual.getMinutes();
+let segundo = dataAtual.getSeconds();
+
+const usuarios = [];
 
 let novoUsuario;
 
+//pega o nome do usuario e envia para o servidor
 function cadastrarUsuario(){
 
     let perguntaNome = prompt("Escolha um nome de usuário")
     let nomeDeUsuario = {name: perguntaNome}
 
     novoUsuario = perguntaNome
-
-    console.log('executando')
-    
+  
     const usuario = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nomeDeUsuario)
     usuario.then(cadastrado)
     usuario.catch(naoCadastrado)
 }
 
+//retorna o erro e pede outro nome
 function naoCadastrado(erro) {
-
-    console.log(erro.response.status);
 
     if ( erro.response.status === 400){
         console.log("funcionando")
@@ -31,110 +33,68 @@ function naoCadastrado(erro) {
         return
     }
     else {
-        alert('deu errado')
+        console.log('deu errado')
     }
  }
 
+ //insere html com a notificação de entrada do usuario
  function cadastrado(){
-
-    const usuarioEntrou = document.querySelector('.espaco-chat')
-    usuarioEntrou.innerHTML = `<li class="notificacao entrada"> <span class="negrito">${novoUsuario}</span> entra na sala... </li>`
+    console.log('tudo certo')
+    // const usuarioEntrou = document.querySelector('.espaco-chat')
+    // usuarioEntrou.innerHTML = `<li class="notificacao status">
+    // <span class="cinza">(${hora}:${minuto}:${segundo})</span> <span class="negrito"> ${novoUsuario}</span> entra na sala... 
+    // </li>`
  }
 
  cadastrarUsuario()
 
+ //mantem o status do usuario atualizado
+ function online(){
+    let nomeDeUsuario = {name: novoUsuario}
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeDeUsuario)
+}
 
+setInterval(online, 5000);
 
-// function pegarReceitas() {
-//     // 1 - tenho que ter uma ferramenta que faz isso tudo funcionar -> axios
+//buscar mensagens no servidor
+function buscarMensagem(){
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promessa.then(chegou)
+    promessa.catch(nChegou)
+}
 
-//     // 2 - enviar a cartinha pedindo para pegar as receitas salvas
-    
-//     const promessa = axios.get('https://instructor-api.sistemas.driven.com.br/projects/tastecamp/receitas');
-//     // console.log('Enviou o pedido');
-//     // 3 - receber a resposta do servidor
-//     promessa.then(respostaChegou); // agendando para a função respostaChegou ser esecuta quando a resposta do servidor chegar 
-    
-//     //setTimeout(respostaChegou, 5000);
-// }
+buscarMensagem();
 
-// pegarReceitas()
+let mensagens = [];
 
+function chegou(resposta) {
+    renderizarMensagens(resposta)
+} 
 
-// function respostaChegou(resposta) {
-//     // console.log("RESPOSTA CHEGOU");
-//     // console.log(resposta);
-//     console.log(resposta);
+function renderizarMensagens(notific){
+    const tudo = notific.data
 
-//     receitas = resposta.data;
+    for(let i=0; tudo.length > i; i++){
+        mensagens.push(tudo[i])
+    }
 
-//     // 4 - renderizar as receitas vindas do servidor
-//     rederizarReceitas();
-// }
+    adicionarNaTela()
+}
 
+function adicionarNaTela(){
+    const msg = document.querySelector('.espaco-chat');
 
-// function adicinarReceita() {
-//     // pegar o info digitada nos inputs
-//     const nomeReceita = document.querySelector('.nome-receita').value;
-//     const ingredientesReceita = document.querySelector('.ingredientes-receita').value;
-//     const preparoReceitas = document.querySelector('.modo-preparo-receita').value;
+    for(let i=0; mensagens.length>i; i++){
+        msg.innerHTML += `
+        <li class="${mensagens[i].type} notificacao">
+            <span class="cinza"> (${mensagens[i].time}) </span> <span class="negrito">${mensagens[i].from}</span> para <span class="negrito">${mensagens[i].to}</span> : ${mensagens[i].text}
+        </li>
+    `
+    }
+}
 
-//     // criar um objeto com essas informações
-//     const novaReceita = {
-//         titulo: nomeReceita,
-//         ingredientes: ingredientesReceita,
-//         preparo: preparoReceitas
-//     };
-//     console.log(novaReceita);
+setInterval(buscarMensagem, 3000);
 
-//     // adicionar a nova receita (objeto) no array
-//     // receitas.push(novaReceita);
-//     const promise = axios.post("https://instructor-api.sistemas.driven.com.br/projects/tastecamp/receitas", novaReceita);
-//     promise.then(deuCerto)      // agenda uma função para quando a requisição dá certo
-//     promise.catch(deuErrado)    // agenda uma função para quando a requisição dá errado
-
-//     // renderizar as receitas na tela
-//     rederizarReceitas();
-// }
-
-// function deuCerto(resposta) {
-//     console.log(resposta.status)
-//     alert("Sua receita foi adicionada!")
-//     pegarReceitas()
-// }
-
-// function deuErrado(erro) {
-//     console.log(erro);
-    
-//     if ( erro.response.status === 422){
-//         alert(erro.response.data.detalhes[0]+', '+erro.response.data.detalhes[1]);
-//     }else if ( erro.response.status === 404) {
-//         alert("O recurso não foi encontrada no servidor");
-//     }else{
-//         alert("ocorreu um erro!");
-//     }
-// }
-
-// function rederizarReceitas() {
-
-//     const listaReceitas = document.querySelector('.receitas');
-
-//     listaReceitas.innerHTML = '';
-
-//     for (let i = 0; i < receitas.length; i++) {
-
-//         let receita = receitas[i];
-
-//         listaReceitas.innerHTML += `
-//             <li>
-//                 <ion-icon name="fast-food-outline"></ion-icon>
-//                 ${receita.titulo}
-//             </li>
-//         `;
-//     }
-
-// }
-// rederizarReceitas();
-
-
-
+function nChegou(resposta){
+    console.log(resposta)
+}
